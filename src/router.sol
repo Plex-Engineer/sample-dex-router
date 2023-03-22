b85936bf794a1d48de5af336100adde5c0fe35f6
T.K. Kwon <tkkwon1998@gmail.com>
2023-03-22 17:51:01 -0400

    method addLiquidity

---
+++ src/router.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.8.18;
+
+import "./interfaces/IUniswapV2Factory.sol";
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IUniswapV2ERC20.sol";
+import "forge-std/StdMath.sol";
+
+contract Router {
+    //state variables
+    IUniswapV2Factory factory;
+
+    //constructor
+    constructor(address _factory) {
+        factory = IUniswapV2Factory(_factory);
+    }
+
+    //methods
+
+    //add liquidity 
+    function addLiquidity(address tokenA, address tokenB, uint amtA, uint amtB, uint slippageRatio) public returns (uint) {
+        // which pool?
+        IUniswapV2Pair pair = IUniswapV2Pair(factory.getPair(tokenA, tokenB));
+        if (address(pair) == address(0)) {
+            //create pair
+            pair = IUniswapV2Pair(factory.createPair(tokenA, tokenB));
+        } else {
+            // getReserves
+            (uint112 reserveA, uint112 reserveB, ) = pair.getReserves();
+            
+            // if abs( amtA/amtB - realReserveRatio ) > slippageRatio, revert
+            uint userRatio = amtA * 1e18 / amtB;
+            uint realRatio = reserveA * 1e18 / reserveB;
+            uint delta = stdMath.percentDelta(userRatio, realRatio);
+            require(delta > slippageRatio, "Router::addLiquidity: Slippage is too high"); 
+        }
+
+        //add liquidity to new pair
+        IUniswapV2ERC20(tokenA).transferFrom(msg.sender, address(pair), amtA);
+        IUniswapV2ERC20(tokenB).transferFrom(msg.sender, address(pair), amtB);
+
+        // mint LP tokens
+        IUniswapV2Pair(pair).mint(msg.sender);
+    } 
+
+    //remove liquidity 
+
+    //swap
+
+    //_create pair
+}
---
+++ test/Router.t.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+contract SetUp is Test{
+
+    function setUp() public virtual{
+        
+    }
+}
+
+contract RouterTest is SetUp {
+    uint test;
+    
+}
