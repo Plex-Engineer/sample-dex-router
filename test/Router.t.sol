41dca6e20fa6baa2bfcb4434b2a65e486252b4af
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2023-03-23 12:40:21 -0400

    multiple add liquidity test

--- test/Router.t.sol
+++ test/Router.t.sol
@@ -26,23 +26,82 @@ contract SetUp is Test {
 }
 
 contract RouterTest is SetUp {
-    function testAddLiquidity() public {
+    function testNeedAllowance() public {
         vm.startPrank(tester);
-        testToken1.approve(address(router), 100 ether);
-        testToken2.approve(address(router), 100 ether);
-        uint expectedLiquidity = router.addLiquidity(
+        //will revert since no allowance was set
+        vm.expectRevert();
+        router.addLiquidity(
             address(testToken1),
             address(testToken2),
             50 ether,
             50 ether,
             10
         );
+        vm.stopPrank();
+    }
+
+    function testAddLiquidity() public {
+        vm.startPrank(tester);
+        testToken1.approve(address(router), 1000 ether);
+        testToken2.approve(address(router), 1000 ether);
+        uint expectedLiquidity = router.addLiquidity(
+            address(testToken1),
+            address(testToken2),
+            10 ether,
+            10 ether,
+            10 ether
+        );
 
         address pair = factory.getPair(
             address(testToken1),
             address(testToken2)
         );
+        vm.stopPrank();
         assert(pair != address(0));
         assertEq(IUniswapV2Pair(pair).balanceOf(tester), expectedLiquidity);
     }
+
+    function testMultipleAddLiquidity() public {
+        vm.startPrank(tester);
+        testToken1.approve(address(router), 1000 ether);
+        testToken2.approve(address(router), 1000 ether);
+        uint expectedLiquidity1 = router.addLiquidity(
+            address(testToken1),
+            address(testToken2),
+            10 ether,
+            10 ether,
+            10 ether
+        );
+        uint expectedLiquidity2 = router.addLiquidity(
+            address(testToken1),
+            address(testToken2),
+            10 ether,
+            10 ether,
+            10 ether
+        );
+        vm.stopPrank();
+        address pair = factory.getPair(
+            address(testToken1),
+            address(testToken2)
+        );
+        assertEq(
+            IUniswapV2Pair(pair).balanceOf(tester),
+            expectedLiquidity1 + expectedLiquidity2
+        );
+    }
+
+    // function testAddLiquidityWithWrongRatio() public {
+    //     //add initial liquidity into pool
+    //     testAddLiquidity();
+    //     //pool ratio is 1:1, we will try 2:1
+    //     // vm.expectRevert();
+    //     vm.startPrank(tester);
+    //     router.addLiquidity(
+    //         address(testToken1),
+    //         address(testToken2),
+    //         20 ether,
+    //         10 ether,
+    //         10 ether
+    //     );
+    // }
 }
